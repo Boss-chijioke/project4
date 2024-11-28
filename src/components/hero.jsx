b70bc2b1aafbcf8496e0530c736554e7cf224f83
boss-chijioke <ojimaduchijioke999@gmail.com>
2024-11-28 14:01:05 +0100

    project4

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,12 +1,12 @@
 import p801 from "../assets/p801.png"
 function Hero(){
     return(
-        <div className="flex border">
+        <div className="flex ">
             
             
             <div className=" flex-1  flex relative h-64 justify-center   ">
                 <img src={p801} alt="p801" className="flex w-full h-44 absolute -z-90" />
-               <div className="flex "><p className="flex  mt-10 "><marquee> <b></b>your smile our passion</marquee> </p></div> 
+               <div className="flex "><p className="flex  mt-10 "><marquee> <b>your smile our passion</b></marquee> </p></div> 
             
                 
                 
